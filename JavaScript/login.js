41d72c42b7123d31830d2b75b369e7c6a4a2a874
juan <jguzmanpenagos@gmail.com>
2025-04-05 00:23:14 -0500

    Modificados archivos de estilo y scripts, eliminados archivos no necesarios y agregados nuevos

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,6 +1,17 @@
+//inicio-----------------------
 function pruebaContinuar(){
-    alert("¡Bienvenido!");
+    alert("¡Bienvenido, podras realizar comprar con FLY-SAVER!");
     confirmacion = document.getElementById("confirmacion").value;
-    window.location.href = "#";
+    window.location.href = "https://www.youtube.com/watch?v=X1e724AA-wU"; 
 
 }
+//Registro-----------------------
+document.getElementById('registro-form').addEventListener('submit', function(event) {
+    const password = document.getElementById('contraseña').value;
+    const confirmPassword = document.getElementById('confirmar-contraseña').value;
+
+    if (password !== confirmPassword) {
+        alert('Las contraseñas no coinciden.');
+        event.preventDefault(); // Evita el envío del formulario
+    }
+});
